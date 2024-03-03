ad1f04fe628a4004a1a844a1d2e604f0ef00e3a5
Tabitha Buder <tabibuder@gmail.com>
2024-03-02 22:04:20 -0800

    added mainMenu function and installed dotenv

--- db/index.js
+++ db/index.js
@@ -138,5 +138,47 @@ function updateEmployeeRole(){
 }
 
 function mainMenu(){
-
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'option',
+            message: 'Select an option:',
+            choices: [
+            'View all departments',
+            'View all roles',
+            'View all employees',
+            'Add a department',
+            'Add a role',
+            'Add an employee',
+            'Update an employee role'
+            ]
+        }
+        ]).then(answer => {
+        switch (answer.option) {
+            case 'View all departments':
+            viewDepartments();
+            break;
+            case 'View all roles':
+            viewRoles();
+            break;
+            case 'View all employees':
+            viewEmployees();
+            break;
+            case 'Add a department':
+            addDepartment();
+            break;
+            case 'Add a role':
+            addRole();
+            break;
+            case 'Add an employee':
+            addEmployee();
+            break;
+            case 'Update an employee role':
+            updateEmployeeRole();
+            break;
+            default:
+            console.log('Invalid option');
+            break;
+        }
+    });
 }
